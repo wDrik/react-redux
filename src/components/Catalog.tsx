92b392d2f13df8d0c5113380bbfee452dc70b828
Iorgen Wildrik Souza da Silva <iorgen.silva@sysmap.com.br>
2020-10-29 18:43:56 -0300

    feat: add cart

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-import { useSelector } from 'react-redux';
+import { IProduct } from '../store/modules/cart/types';
+import api from '../services/api';
+import { addProductToCart } from '../store/modules/cart/actions';
 
 const Catalog: React.FC = () => {
-  const catalog = useSelector(state => state);
+  const dispatch = useDispatch();
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-  console.log(catalog);
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data)
+    })
+  }, []);
 
-  return <h1>Catalog</h1>;
+  const handleAddProductToCart = useCallback((product: IProduct) => {
+    dispatch(addProductToCart(product))
+  }, [dispatch])
+
+  return (
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {' - '}
+          <span>{product.price}</span> {'  '}
+
+          <button type="button" onClick={() => handleAddProductToCart(product)}>Comprar</button>
+        </article>
+      ))}
+    </main>
+  );
 }
 
 export default Catalog;
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -5,8 +5,24 @@ const INITIAL_STATE: ICartState = {
   items: []
 }
 
-const cart: Reducer<ICartState, null> = () => {
-  return INITIAL_STATE
+const cart: Reducer<ICartState, any> = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'ADD_PRODUCT_TO_CART': {
+      const { product } = action.payload;
+
+      state.items.push({
+        product,
+        quantity: 1
+      })
+
+      return state;
+
+      break;
+    }
+    default: {
+      return state;
+    }
+  }
 }
 
 export default cart;
