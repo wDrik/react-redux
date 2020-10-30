693a0c0b57aecde8c61033342b747fd4339e6c09
Iorgen Wildrik Souza da Silva <iorgen.silva@sysmap.com.br>
2020-10-30 08:19:26 -0300

    feat: get current quantity item

---
+++ src/store/modules/cart/sagas.ts
@@ -0,0 +1,20 @@
+import { all, select, takeLatest } from 'redux-saga/effects';
+
+import { addProductToCart } from './actions';
+import { IState } from './../../index';
+
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+  })
+
+  console.log(currentQuantity)
+}
+
+export default all([
+  takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)
+])
