83863526069e1c59efec4d1eb960e9efb441d1a4
Iorgen Wildrik Souza da Silva <iorgen.silva@sysmap.com.br>
2020-10-30 06:56:37 -0300

    feat: add immer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -8,21 +8,34 @@ const INITIAL_STATE: ICartState = {
 }
 
 const cart: Reducer<ICartState, any> = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case 'ADD_PRODUCT_TO_CART': {
-      const { product } = action.payload;
+  return produce(state, draft => {
+    switch (action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload;
 
-      return produce(state, draft => {
-        draft.items.push({
-          product,
-          quantity: 1
-        })
-      })
+        const productInCartIndex = draft.items.findIndex(item =>
+          item.product.id === product.id
+        );
+
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity++;
+        } else {
+          draft.items.push({
+            product,
+            quantity: 1
+          })
+        }
+
+
+
+        break;
+      }
+      default: {
+        return state;
+      }
     }
-    default: {
-      return state;
-    }
-  }
+  })
+
 }
 
 export default cart;
